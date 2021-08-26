401553a55a83572fd44d372573e5335b7e6eb554
Job.Sluis <Job.Sluis@outlook.com>
2021-08-26 19:42:47 +0200

    Create parallax effect for the parallax images.

---
+++ Scripts/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+    this.querySelectorAll('.parallax-item').forEach(item => {
+        const speed = item.getAttribute('data-speed')
+        const x = (window.innerWidth - e.pageX * speed)/100
+        const y = (window.innerHeight - e.pageY * speed)/100
+        item.style.transform = 'translateX('+x+'px) translateY('+y+'px)'
+    });
+}
